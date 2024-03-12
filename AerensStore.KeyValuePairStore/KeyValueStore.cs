8de564b22d40b2be3a0ec2cdc666cf6a74383d4c
Laurent Aerens <Laurent.Aerens@codit.eu>
2024-03-12 17:59:43 +0100

    a option to interpter your key without processing during set

--- AerensStore.KeyValuePairStore/KeyValueStore.cs
+++ AerensStore.KeyValuePairStore/KeyValueStore.cs
@@ -92,7 +92,7 @@ public class KeyValueStore
             throw new ArgumentException("The settings in the keyValueStore en the contructor do not match, Constructor onces are taken: This might brake stuff.");
         }
     }
-    public DeltaTime CreateDeltaTimeFromJson(string json)
+    private DeltaTime CreateDeltaTimeFromJson(string json)
     {
         var jObject = JObject.Parse(json);
 
@@ -129,7 +129,7 @@ public class KeyValueStore
     public List<string> GetKeys(bool WithDateTime = false)
     {
         int[] timeToKeep = ContinuesStoreTime.GetTimeValues();
-        if (WithDateTime)
+        if (WithDateTime || ContinuesStoreTime.IsOff)
         {
             return store.Keys.ToList();
         }
@@ -205,6 +205,10 @@ public class KeyValueStore
     /// <returns>A dictionary containing Iterations and Date for the key.</returns>
     public Dictionary<int, string> GetKeyVersions(string keyName)
     {
+        if (ContinuesStoreTime.IsOff)
+        {
+            return null;
+        }
         Dictionary<int, string> keyVersions = new Dictionary<int, string>();
         int[] timeToKeep = ContinuesStoreTime.GetTimeValues();
         foreach (string key in store.Keys)
@@ -244,6 +248,15 @@ public class KeyValueStore
     /// <returns>A dictionary containing the keys and their versions.</returns>
     public Dictionary<string, List<string>> GetKeysVersions()
     {
+        if (ContinuesStoreTime.IsOff)
+        {
+            Dictionary<string, List<string>> keysNoVersions = new Dictionary<string, List<string>>();
+            foreach (string key in store.Keys)
+            {
+                keysNoVersions[key] = null;
+            }
+            return keysNoVersions;
+        }
         int[] timeToKeep = ContinuesStoreTime.GetTimeValues();
         Dictionary<string, List<string>> keyVersions = new Dictionary<string, List<string>>();
         if (timeToKeep[0] != 0)
@@ -308,9 +321,10 @@ public class KeyValueStore
     /// </summary>
     /// <param name="key">The key to associate the value with.</param>
     /// <param name="value">The value to be stored.</param>
-    public void Set(string key, object value)
+    /// <param name="straightText">A flag that allows you to turn of the keyDeltaTime Proceccing</param>
+    public void Set(string key, object value, bool straightText = false)
     {
-        if (ContinuesStoreTime.IsOff)
+        if (ContinuesStoreTime.IsOff || straightText)
         {
             store[key] = value;
             Save();
@@ -337,16 +351,16 @@ public class KeyValueStore
     /// <param name="key">The key of the value to retrieve.</param>
     /// <param name="iterationsAgo">The number of iterations ago to search for the key.</param>
     /// <param name="lookupDate">The lookup date get the keyValue on a certain date</param>
-    /// <param name="StraightLookup">Removes all DateTime processing and look the key as is</param>
+    /// <param name="straightLookup">Removes all DateTime processing and look the key as is</param>
     /// <returns>
     /// The value associated with the specified key, or null if the key is not found.
     /// </returns>
-    public object Get(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool StraightLookup = false)
+    public object Get(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool straightLookup = false)
     {
         try
         {
             object result = null;
-            if (ContinuesStoreTime.IsOff || StraightLookup)
+            if (ContinuesStoreTime.IsOff || straightLookup)
             {
                 return store[key];
             }
@@ -374,9 +388,10 @@ public class KeyValueStore
     /// </summary>
     /// <param name="key">The key to set.</param>
     /// <param name="value">The value to set.</param>
-    public void SetString(string key, string value)
+    /// <param name="straightText">A flag that allows you to turn of the keyDeltaTime Proceccing</param>
+    public void SetString(string key, string value, bool straightText = false)
     {
-        Set(key, value);
+        Set(key, value, straightText);
     }
     /// <summary>
     /// Retrieves the value associated with the specified key as a string.
@@ -387,13 +402,13 @@ public class KeyValueStore
     /// If the value from the specified number of iterations ago does not exist, an error is thrown instead of returning empty string.
     /// </param>
     ///<param name="lookupDate">The lookup date get the keyValue on a certain date</param>
-    /// <param name="StraightLookup">Removes all DateTime processing and look the key as is</param>
+    /// <param name="straightLookup">Removes all DateTime processing and look the key as is</param>
     /// <returns>The value associated with the specified key as a string, or empty string if the key does not exist or the value is not a string.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the key does not exist in the store and iterationsAgo != 0.</exception>
     /// <exception cref="InvalidCastException">Thrown when the value associated with the key is not a string.</exception>
-    public string GetString(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool StraightLookup = false)
+    public string GetString(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool straightLookup = false)
     {
-        var value = Get(key, iterationsAgo, lookupDate, StraightLookup);
+        var value = Get(key, iterationsAgo, lookupDate, straightLookup);
         if (value == null)
         {
             return string.Empty;
@@ -413,9 +428,10 @@ public class KeyValueStore
     /// </summary>
     /// <param name="key">The key to set.</param>
     /// <param name="value">The integer value to set.</param>
-    public void SetInt(string key, int value)
+    /// <param name="straightText">A flag that allows you to turn of the keyDeltaTime Proceccing</param>
+    public void SetInt(string key, int value, bool straightText = false)
     {
-        Set(key, value);
+        Set(key, value, straightText);
     }
 
     /// <summary>
@@ -427,13 +443,13 @@ public class KeyValueStore
     /// If the value from the specified number of iterations ago does not exist, an error is thrown instead of returning 0.
     /// </param>
     /// <param name="lookupDate">The lookup date get the keyValue on a certain date</param>
-    /// <param name="StraightLookup">Removes all DateTime processing and look the key as is</param>
+    /// <param name="straightLookup">Removes all DateTime processing and look the key as is</param>
     /// <returns>The value associated with the specified key as a integer, or 0 if the key does not exist or the value is not a integer.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the key does not exist in the store and iterationsAgo != 0.</exception>
     /// <exception cref="InvalidCastException">Thrown when the value associated with the key is not a integer.</exception>
-    public int GetInt(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool StraightLookup = false)
+    public int GetInt(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool straightLookup = false)
     {
-        var value = Get(key, iterationsAgo, lookupDate, StraightLookup);
+        var value = Get(key, iterationsAgo, lookupDate, straightLookup);
         if (value == null)
         {
             return 0;
@@ -465,9 +481,10 @@ public class KeyValueStore
     /// </summary>
     /// <param name="key">The key to set.</param>
     /// <param name="value">The double value to set.</param>
-    public void SetDouble(string key, double value)
+    /// <param name="straightText">A flag that allows you to turn of the keyDeltaTime Proceccing</param>
+    public void SetDouble(string key, double value, bool straightText = false)
     {
-        Set(key, value);
+        Set(key, value, straightText);
     }
 
     /// <summary>
@@ -479,13 +496,13 @@ public class KeyValueStore
     /// If the value from the specified number of iterations ago does not exist, an error is thrown instead of returning 0.
     /// </param>
     /// <param name="lookupDate">The lookup date get the keyValue on a certain date</param>
-    /// <param name="StraightLookup">Removes all DateTime processing and look the key as is</param>
+    /// <param name="straightLookup">Removes all DateTime processing and look the key as is</param>
     /// <returns>The value associated with the specified key as a double, or 0 if the key does not exist or the value is not a double.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the key does not exist in the store and iterationsAgo != 0.</exception>
     /// <exception cref="InvalidCastException">Thrown when the value associated with the key is not a double.</exception>
-    public double GetDouble(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool StraightLookup = false)
+    public double GetDouble(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool straightLookup = false)
     {
-        var value = Get(key, iterationsAgo, lookupDate, StraightLookup);
+        var value = Get(key, iterationsAgo, lookupDate, straightLookup);
         if (value == null)
         {
             return 0;
@@ -505,9 +522,10 @@ public class KeyValueStore
     /// </summary>
     /// <param name="key">The key to set.</param>
     /// <param name="value">The long value to set.</param>
-    public void SetLong(string key, long value)
+    /// <param name="straightText">A flag that allows you to turn of the keyDeltaTime Proceccing</param>
+    public void SetLong(string key, long value, bool straightText = false)
     {
-        Set(key, value);
+        Set(key, value, );
     }
 
     /// <summary>
@@ -519,13 +537,13 @@ public class KeyValueStore
     /// If the value from the specified number of iterations ago does not exist, an error is thrown instead of returning 0.
     /// </param>
     /// <param name="lookupDate">The lookup date get the keyValue on a certain date</param>
-    /// <param name="StraightLookup">Removes all DateTime processing and look the key as is</param>
+    /// <param name="straightLookup">Removes all DateTime processing and look the key as is</param>
     /// <returns>The value associated with the specified key as a long, or 0 if the key does not exist or the value is not a long.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the key does not exist in the store and iterationsAgo != 0.</exception>
     /// <exception cref="InvalidCastException">Thrown when the value associated with the key is not a long.</exception>
-    public long GetLong(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool StraightLookup = false)
+    public long GetLong(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool straightLookup = false)
     {
-        var value = Get(key, iterationsAgo, lookupDate, StraightLookup);
+        var value = Get(key, iterationsAgo, lookupDate, straightLookup);
         if (value == null)
         {
             return 0;
@@ -545,9 +563,10 @@ public class KeyValueStore
     /// </summary>
     /// <param name="key">The key to set.</param>
     /// <param name="value">The char value to set.</param>
-    public void SetChar(string key, char value)
+    /// <param name="straightText">A flag that allows you to turn of the keyDeltaTime Proceccing</param>
+    public void SetChar(string key, char value, bool straightText = false)
     {
-        Set(key, value);
+        Set(key, value, straightText);
     }
 
     /// <summary>
@@ -559,13 +578,13 @@ public class KeyValueStore
     /// If the value from the specified number of iterations ago does not exist, an error is thrown instead of returning '\0'.
     /// </param>
     /// <param name="lookupDate">The lookup date get the keyValue on a certain date</param>
-    /// <param name="StraightLookup">Removes all DateTime processing and look the key as is</param>
+    /// <param name="straightLookup">Removes all DateTime processing and look the key as is</param>
     /// <returns>The value associated with the specified key as a char, or '\0' if the key does not exist or the value is not a char.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the key does not exist in the store and iterationsAgo != 0.</exception>
     /// <exception cref="InvalidCastException">Thrown when the value associated with the key is not a char.</exception>
-    public char GetChar(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool StraightLookup = false)
+    public char GetChar(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool straightLookup = false)
     {
-        var value = Get(key, iterationsAgo, lookupDate, StraightLookup);
+        var value = Get(key, iterationsAgo, lookupDate, straightLookup);
         if (value == null)
         {
             return '\0';
@@ -600,9 +619,10 @@ public class KeyValueStore
     /// </summary>
     /// <param name="key">The key to set.</param>
     /// <param name="value">The bool value to set.</param>
-    public void SetBool(string key, bool value)
+    /// <param name="straightText">A flag that allows you to turn of the keyDeltaTime Proceccing</param>
+    public void SetBool(string key, bool value, bool straightText = false)
     {
-        Set(key, value);
+        Set(key, value, straightText);
     }
 
     /// <summary>
@@ -614,13 +634,13 @@ public class KeyValueStore
     /// If the value from the specified number of iterations ago does not exist, an error is thrown instead of returning false.
     /// </param>
     /// <param name="lookupDate">The lookup date get the keyValue on a certain date</param>
-    /// <param name="StraightLookup">Removes all DateTime processing and look the key as is</param>
+    /// <param name="straightLookup">Removes all DateTime processing and look the key as is</param>
     /// <returns>The value associated with the specified key as a bool, or false if the key does not exist or the value is not a bool.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the key does not exist in the store and iterationsAgo != 0.</exception>
     /// <exception cref="InvalidCastException">Thrown when the value associated with the key is not a bool.</exception>
-    public bool GetBool(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool StraightLookup = false)
+    public bool GetBool(string key, int iterationsAgo = 0, DateTime lookupDate = default, bool straightLookup = false)
     {
-        var value = Get(key, iterationsAgo, lookupDate, StraightLookup);
+        var value = Get(key, iterationsAgo, lookupDate, straightLookup);
         if (value == null)
         {
             return false;
